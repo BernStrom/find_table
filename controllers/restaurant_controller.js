6c4b655e725e043d419aaef824c11ab1ce065acf
Bern <bernnystrom@gmail.com>
2020-11-18 09:38:11 +0800

    Change to restricted API key

--- controllers/restaurant_controller.js
+++ controllers/restaurant_controller.js
@@ -2,13 +2,13 @@ const axios = require("axios");
 const api_key = process.env.GOOGLE_MAPS_API_KEY;
 function restaurantsLocation(userLat, userLog) {
   return axios.get(
-    `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${userLat},${userLog}&radius=6000&type=restaurant&keyword=burger&key=${api_key}`
+    `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${userLat},${userLog}&radius=6000&type=restaurant&keyword=burger&key=AIzaSyD35x-cTKapedpyxkHXy2VR2CD0xJzRC1Q`
   );
 }
 
 function restaurantsDetails(place_id) {
   return axios.get(
-    `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&key=${api_key}`
+    `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&key=AIzaSyD35x-cTKapedpyxkHXy2VR2CD0xJzRC1Q`
   );
 }
 
