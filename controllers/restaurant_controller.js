4f6942c1bda3167f22cc5bab44422d7f92f7cfbb
Bern <bernnystrom@gmail.com>
2020-11-18 09:27:34 +0800

    Secure map API keys

--- controllers/restaurant_controller.js
+++ controllers/restaurant_controller.js
@@ -2,13 +2,13 @@ const axios = require("axios");
 const api_key = process.env.GOOGLE_MAPS_API_KEY;
 function restaurantsLocation(userLat, userLog) {
   return axios.get(
-    `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${userLat},${userLog}&radius=6000&type=restaurant&keyword=burger&key=AIzaSyABL4y3J8mON1Db0z4-C-ExwOSZ-W-VHrY`
+    `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${userLat},${userLog}&radius=6000&type=restaurant&keyword=burger&key=${api_key}`
   );
 }
 
 function restaurantsDetails(place_id) {
   return axios.get(
-    `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&key=AIzaSyABL4y3J8mON1Db0z4-C-ExwOSZ-W-VHrY`
+    `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&key=${api_key}`
   );
 }
 
