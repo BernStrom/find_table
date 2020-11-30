cd2e2059e5786a2bc10e98140a1ab7fa6c6e6e7d
Bern <bernnystrom@gmail.com>
2020-11-30 16:11:32 +0800

    Change to env API key and link to deployed app

--- controllers/restaurant_controller.js
+++ controllers/restaurant_controller.js
@@ -1,19 +1,17 @@
 require('dotenv').config();
-
 const axios = require("axios");
 const api_key = process.env.GOOGLE_MAPS_API_KEY;
-console.log(api_key);
 
 function restaurantsLocation(userLat, userLog) {
   return axios.get(
-    `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${userLat},${userLog}&radius=6000&type=restaurant&keyword=burger&key=AIzaSyAg6Nm4Yu6w2hfIuI3stQZcdZMuPO0XlUM`
+    `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${userLat},${userLog}&radius=6000&type=restaurant&keyword=burger&key=${api_key}`
   );
 }
 
 function restaurantsDetails(place_id) {
   return axios.get(
-    `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&key=AIzaSyAg6Nm4Yu6w2hfIuI3stQZcdZMuPO0XlUM`
-  )
+    `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&key=${api_key}`
+  );
 }
 
 module.exports = {
