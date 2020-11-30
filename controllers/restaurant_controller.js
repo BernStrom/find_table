2b17a900ac3eb1bc4484fe6a050dbb213bf2662f
Bern <bernnystrom@gmail.com>
2020-11-30 15:09:56 +0800

    Fix env api

--- controllers/restaurant_controller.js
+++ controllers/restaurant_controller.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
+
 const axios = require("axios");
 const api_key = process.env.GOOGLE_MAPS_API_KEY;
+console.log(api_key);
+
 function restaurantsLocation(userLat, userLog) {
   return axios.get(
     `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${userLat},${userLog}&radius=6000&type=restaurant&keyword=burger&key=${api_key}`
